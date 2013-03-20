28bd2cd1fc9e01ff995d7bce552d7b4403992d71
Tanner Smith <tanner@tssoftware.net>
2013-03-20 13:41:42 -0400

    Implement turning the genome into code.

--- src/brainfuck/genetic/entity.go
+++ src/brainfuck/genetic/entity.go
@@ -2,6 +2,7 @@ package genetic
 
 import (
 	"math/rand"
+	"bytes"
 )
 
 const numberGenes int = 100
@@ -21,4 +22,34 @@ func NewEntity() *Entity {
 	e := Entity{genome}
 
 	return &e
+}
+
+func (e *Entity) Code() string {
+	var code bytes.Buffer
+	var instruction rune
+
+	for _, v := range e.Genome {
+		switch(v) {
+		case 0:
+			instruction = '>'
+		case 1:
+			instruction = '<'
+		case 2:
+			instruction = '+'
+		case 3:
+			instruction = '-'
+		case 4:
+			instruction = '.'
+		case 5:
+			instruction = ','
+		case 6:
+			instruction = '['
+		case 7:
+			instruction = ']'
+		}
+
+		code.WriteRune(instruction)
+	}
+
+	return code.String()
 }
