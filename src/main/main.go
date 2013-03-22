8e933e00948a349574e17e16e5379f3d9d07c438
Tanner Smith <tanner@tssoftware.net>
2013-03-22 17:49:34 -0400

    Enable Go to use 4 processors.

--- src/main/main.go
+++ src/main/main.go
@@ -5,11 +5,14 @@ import (
 	"bytes"
 	"strings"
 	"time"
+	"runtime"
 	"brainfuck"
 	"brainfuck/genetic"
 )
 
 func main() {
+	runtime.GOMAXPROCS(4)
+
 	algorithm := genetic.NewAlgorithm(500, 10, 0.5, "Hi", "", 1000)
 
 	desiredFitness := 0.8
